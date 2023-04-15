8f0d1c444d512d99574461a5a60437da59636584
Usman Ghani Hingoro <64012766+ughingoro@users.noreply.github.com>
2023-04-15 14:54:54 +0500

    server page updated

---
+++ app/server/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+type Book = {
+    id: number,
+    name: string,
+    type: string,
+    available: boolean
+}
+async function getBooks() {
+    const response = await fetch("https://simple-books-api.glitch.me/books", {
+        cache: "no-store",
+    });
+    const data = response.json();
+    return data;
+}
+
+export default async function staticPage() {
+    const books = await getBooks();
+
+    return <div>
+        <h1>Server Page</h1>
+        {books.map((book: Book) =>
+            <ul key={book.id}>
+                <li>
+                    {book.name} - {book.type}
+                </li>
+            </ul>
+        )}
+    </div>
+
+}
+
